6220b1faa9f57025470cc0ad940f705eb2e0d9c8
Alan-Mun <alan.mun.98@mail.ru>
2023-12-07 01:37:39 +0300

    sprint_06

---
+++ MovieQuiz/Models/MostPopularMovies.swift
@@ -0,0 +1,8 @@
+//
+//  MostPopularMovies.swift
+//  MovieQuiz
+//
+//  Created by Алан Мун on 07.12.2023.
+//
+
+import Foundation
---
+++ MovieQuiz/Services/NetworkClient.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkClient.swift
+//  MovieQuiz
+//
+//  Created by Алан Мун on 07.12.2023.
+//
+
+import Foundation
