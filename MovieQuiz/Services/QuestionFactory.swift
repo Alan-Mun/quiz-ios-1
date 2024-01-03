7351acdacceee43018008c3dc4fdd8998f555363
Alan-Mun <alan.mun.98@mail.ru>
2024-01-04 00:56:15 +0300

    sprint_07

--- MovieQuiz/Services/QuestionFactory.swift
+++ MovieQuiz/Services/QuestionFactory.swift
@@ -27,38 +27,7 @@ final class QuestionFactory: QuestionFactoryProtocol {
         }
     }
     
-    /*private let questions: [QuizQuestion] = [
-     QuizQuestion(
-     image: "The Godfather",
-     correctAnswer: true),
-     QuizQuestion(
-     image: "The Dark Knight",
-     correctAnswer: true),
-     QuizQuestion(
-     image: "Kill Bill",
-     correctAnswer: true),
-     QuizQuestion(
-     image: "The Avengers",
-     correctAnswer: true),
-     QuizQuestion(
-     image: "Deadpool",
-     correctAnswer: true),
-     QuizQuestion(
-     image: "The Green Knight",
-     correctAnswer: true),
-     QuizQuestion(
-     image: "Old",
-     correctAnswer: false),
-     QuizQuestion(
-     image: "The Ice Age Adventures of Buck Wild",
-     correctAnswer: false),
-     QuizQuestion(
-     image: "Tesla",
-     correctAnswer: false),
-     QuizQuestion(
-     image: "Vivarium",
-     correctAnswer: false)
-     ]*/
+
     func requestNextQuestion() {
         DispatchQueue.global().async { [weak self] in
             guard let self = self else { return }
